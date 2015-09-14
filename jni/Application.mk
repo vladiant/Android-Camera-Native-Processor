6b453a82e5faf1df18c362c98b48e2f1a075aaeb
Vladislav Antonov <vladislav.antonov@gmail.com>
2015-09-14 22:53:17 +0300

    Optimized setup for native part.

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,1 +1,9 @@
+APP_ABI := armeabi-v7a
+NDK_TOOLCHAIN_VERSION := 4.9
 APP_OPTIM := release
+APP_STL := gnustl_static
+APP_CPPFLAGS := -frtti -fexceptions -pthread -Wall -std=c++11
+APP_CFLAGS := -pthread -Wall
+
+APP_CFLAGS += -march=armv7-a -mfloat-abi=softfp -mfpu=neon
+APP_CPPFLAGS += -march=armv7-a -mfloat-abi=softfp -mfpu=neon
